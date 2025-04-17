27f4eb2eefaec631b38157d9b5404fbc9e31fcb5
Bruno Racobaldo Santos <brunoracobaldo@hotmail.com>
2025-04-17 16:40:15 -0300

    added local storage to save content

--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { CheckList, SearchInput } from "@/components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [searchInputValue, setSearchInputValue] = useState<string>("")
@@ -15,6 +15,17 @@ export default function Home() {
     setOptions(options.filter((_, index) => index !== indexToRemove));
   };
 
+  useEffect(() => {
+    const storedOptions = localStorage.getItem("todo-options");
+    if (storedOptions) {
+      setOptions(JSON.parse(storedOptions));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todo-options", JSON.stringify(options));
+  }, [options]);
+
   return (
     <div className="flex flex-col p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-8 mx-auto items-center">
@@ -26,7 +37,7 @@ export default function Home() {
         />
       </div>
       <div className="flex flex-col gap-[32px] row-start-2 px-6 sm:items-start w-full md:w-[60%] mx-auto">
-        <CheckList options={options} onRemoveItem={onRemoveItem}/>
+        <CheckList options={options} onRemoveItem={onRemoveItem} />
       </div>
     </div>
   );
